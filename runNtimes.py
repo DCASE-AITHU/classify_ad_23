0a4a8ae545a97005cf791a9b0ce845fbfcebeabb
lvzhiqiang <lvzhiqiang1990@qq.com>
2023-04-28 17:36:14 +0800

    update runNtimes.py

--- runNtimes.py
+++ runNtimes.py
@@ -11,14 +11,14 @@ exp='exp_mobilefacenet_2s_sp_eval'
 tempfile = f'temp-{exp}-mt{mt}-{curT}-{task}.csv'
 outResultFile = f'Rs-{exp}-mt{mt}-{curT}-{task}.csv'
 sp="True" #if True, use speed perturbed dataaset
-device=1
+device=2
 max_step=40000
 dur=2.0
 batch_size=32
 accumulate_grad_step=8
 lr=0.001
 for _ in range(nTimes):
-    os.system(f'CUDA_VISIBLE_DEVICES={device} python experiment23_stepdl_w2v.py \
+    os.system(f'CUDA_VISIBLE_DEVICES={device} python experiment23_stepdl.py \
                 -mt {mt} --result_file {tempfile} \
                 --max_step {max_step} --task {task} -dur {dur} \
                 -ep {exp} -bs {batch_size} -ac {accumulate_grad_step} \

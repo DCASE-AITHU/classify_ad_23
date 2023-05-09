47aa2cae77a2d84b001ed3c484dcf6af649756cb
lvzhiqiang <lvzhiqiang1990@qq.com>
2023-05-09 11:17:43 +0800

    update aggregation/main_multi.py

--- aggregation/main_multi.py
+++ aggregation/main_multi.py
@@ -146,81 +146,81 @@ class Model(nn.Module):
         else:
             return output
 
-import sys
-#id=sys.argv[1]
-margin=0.2 #0.2 #0.2 #float(sys.argv[2])
-scale=30 #30 #float(sys.argv[3])
-scp_file=sys.argv[1] #"arks/w2v_300m_2sdev_interval100_step5100_dur2_shift0.5.scp"
-multi_cnt=int(sys.argv[2])
-dataset = Dataset(scp_file, seq_len=3)
+if __name__ == "__main__":
+    import sys
+    margin=0.2
+    scale=30
+    if len(sys.argv) != 2:
+        print("Usage:{} ark_scp run_times".format(sys.argv[0]))
+        exit(0)
+    scp_file=sys.argv[1]
+    multi_cnt=int(sys.argv[2])
+    dataset = Dataset(scp_file, seq_len=3)
+    dataloader = DataLoader(dataset, batch_size = 2048, shuffle=True)
+    input_shapes=[]
+    for i in dataloader:
+        input_shapes = i[0].shape
+        break
+    print(input_shapes)
+    print(dataset.__class_cnt__())
 
-dataloader = DataLoader(dataset, batch_size = 2048, shuffle=True)
-input_shapes=[]
-for i in dataloader:
-    input_shapes = i[0].shape
-    break
-print(input_shapes)
-print(dataset.__class_cnt__())
+    #backends = ['lof', 'maha', 'knn', 'cos']
+    backends = ['knn']
 
-#backends = ['lof', 'maha', 'knn', 'cos']
-backends = ['knn']
-#backends = ['cos']
+    n_epochs = 1
+    models=[]
+    machines_from_scp = {}
+    dev_machines = ['bearing', 'fan', 'gearbox', 'slider', 'ToyCar', 'ToyTrain', 'valve']
+    with open(scp_file, "r") as fin:
+        for line in fin.readlines():
+            machines_from_scp[ line.split("-")[0] ] = 1
+    machines = []
+    for machine in machines_from_scp.keys():
+        if machine in dev_machines:
+            machines.append(machine)
+    print(machines)
+    for try_id in range(multi_cnt):
+        model = Model(embedding_dim=input_shapes[-1], output_dim = dataset.__class_cnt__())
+        model.cuda()
+        optimizer = torch.optim.Adam(
+                    params=filter(lambda p: p.requires_grad, model.parameters()),
+                    lr=0.0001)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2)
+        for n_epoch in range(n_epochs):
+            train_accs = []
+            train_losses = []
+            for batch in dataloader:
+                optimizer.zero_grad()
+                loss, output = model(batch[0].cuda(), batch[1].unsqueeze(1).cuda())
+                train_losses.append(loss.item())
+                loss.backward()
+                optimizer.step()
+                with torch.no_grad():
+                    acc = accuracy(output, batch[1].unsqueeze(1).cuda())
+                    train_accs.append(acc)
+            train_loss = sum(train_losses) / len(train_losses)
+            scheduler.step(train_loss)
+            train_acc = sum(train_accs) / len(train_accs)
+            print('epoch: {}, lr: {} train_loss: {:.4f} train_acc: {:.4f} '.
+                            format(n_epoch, optimizer.param_groups[0]['lr'], train_loss, train_acc))
+            all_hmeans = []
+            for machine in machines:
+                results = asd_score(model, scp_file, machine, backends)
+                all_hmeans.append(results)
+            for i in range(len(backends)):
+                final_hmean = []
+                for j in range(len(machines)):
+                    final_hmean.append(all_hmeans[j][i])
+                print("all_hmean {} {:.4f}".format(backends[i], hmean(final_hmean)))
+        model.eval()
+        models.append(model)
 
-n_epochs = 1
-models=[]
-machines_from_scp = {}
-dev_machines = ['bearing', 'fan', 'gearbox', 'slider', 'ToyCar', 'ToyTrain', 'valve']
-with open(scp_file, "r") as fin:
-    for line in fin.readlines():
-        machines_from_scp[ line.split("-")[0] ] = 1
-machines = []
-for machine in machines_from_scp.keys():
-    if machine in dev_machines:
-        machines.append(machine)
-print(machines)
-#machines = ['bearing', 'fan', 'gearbox', 'slider', 'ToyCar', 'ToyTrain', 'valve']
-for try_id in range(multi_cnt):
-    model = Model(embedding_dim=input_shapes[-1], output_dim = dataset.__class_cnt__())
-    model.cuda()
-    optimizer = torch.optim.Adam(
-                params=filter(lambda p: p.requires_grad, model.parameters()),
-                lr=0.0001)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2)
-    for n_epoch in range(n_epochs):
-        train_accs = []
-        train_losses = []
-        for batch in dataloader:
-            optimizer.zero_grad()
-            loss, output = model(batch[0].cuda(), batch[1].unsqueeze(1).cuda())
-            train_losses.append(loss.item())
-            loss.backward()
-            optimizer.step()
-            with torch.no_grad():
-                acc = accuracy(output, batch[1].unsqueeze(1).cuda())
-                train_accs.append(acc)
-        train_loss = sum(train_losses) / len(train_losses)
-        scheduler.step(train_loss)
-        train_acc = sum(train_accs) / len(train_accs)
-        print('epoch: {}, lr: {} train_loss: {:.4f} train_acc: {:.4f} '.
-                        format(n_epoch, optimizer.param_groups[0]['lr'], train_loss, train_acc))
-        all_hmeans = []
-        for machine in machines:
-            results = asd_score(model, scp_file, machine, backends)
-            all_hmeans.append(results)
-        for i in range(len(backends)):
-            final_hmean = []
-            for j in range(len(machines)):
-                final_hmean.append(all_hmeans[j][i])
-            print("all_hmean {} {:.4f}".format(backends[i], hmean(final_hmean)))
-    model.eval()
-    models.append(model)
-
-all_hmeans = []
-for machine in machines:
-    results = asd_score_models(models, scp_file, machine, backends)
-    all_hmeans.append(results)
-for i in range(len(backends)):
-    final_hmean = []
-    for j in range(len(machines)):
-        final_hmean.append(all_hmeans[j][i])
-    print("all_hmean_models {} {:.4f}".format(backends[i], hmean(final_hmean)))      
+    all_hmeans = []
+    for machine in machines:
+        results = asd_score_models(models, scp_file, machine, backends)
+        all_hmeans.append(results)
+    for i in range(len(backends)):
+        final_hmean = []
+        for j in range(len(machines)):
+            final_hmean.append(all_hmeans[j][i])
+        print("all_hmean_models {} {:.4f}".format(backends[i], hmean(final_hmean)))      

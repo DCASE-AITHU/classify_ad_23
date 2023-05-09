1136f979357c9579659fe74682914aabdf0037bf
lvzhiqiang <lvzhiqiang1990@qq.com>
2023-05-09 11:14:25 +0800

    add embedding extraction and tranformer-based pooling with default KNN backend

---
+++ run_transformer_based_pooling.sh
@@ -0,0 +1,24 @@
+export PYTHONPATH=`pwd`:$PYTHONPATH
+
+## first step: embedding extraction, kaldiio package needed
+ckpt=best_ckpts/results23exp_wav2vec_300m_2seval_interval200_0_condition_step_3600_method_knn_mean_hmean_63.842
+prefix=wav2vec_300m_2seval_interval200_0_condition_step_3600_method_knn_mean_hmean_63.842
+device=0
+model="wav2vec_300m" # model type, defined in w2v.py 
+output_path=arks
+input_duration=2
+shift_duration=0.5
+dataset=2 #0:dev 1:eval 2:dev+eval
+# for mobilefacenet model, using extract_embedding.py, but with less parameters
+CUDA_VISIBLE_DEVICES=$device python extract_embedding_w2v.py --dataset $dataset \
+                                                             -or $output_path \
+                                                             -duration $input_duration \
+                                                             -shift $shift_duration \
+                                                             -ckpt $ckpt \
+                                                             -prefix $prefix \
+                                                             -model $model
+
+## second step: tranformer-based-pooling
+scp=$output_path/$prefix.scp
+n_times=3 # 3 individual runs and append output embedding
+CUDA_VISIBLE_DEVICES=$device python aggregation/main_multi.py  $scp  ${n_times}
